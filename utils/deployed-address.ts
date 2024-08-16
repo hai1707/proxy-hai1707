d21a422bf2bcd5ef8fda5244d73efafec55dfc49
HAI1707 <thanhhai.tht14@gmail.com>
2024-08-17 00:07:28 +0700

    first commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x525d875E1Effa4f71C31b6122EB69384f8a5d497'
+export const ProxyAdmin = '0xac976f484d3E2d52661eb11ADE15a2c2B4E30414'
+export const SWTRImplementation = '0x6EA79c0D556f14cB6191bfE72E985eaBb8541bae'
+    
